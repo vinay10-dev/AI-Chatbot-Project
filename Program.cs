41610bb5bfb4204a943b4a8da13b6ad403747f15
vinay10-dev <vinaysingh769@gmail.com>
2026-03-30 16:36:47 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Railway se Port uthao, agar nahi hai toh 8080 use karo
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
+// Batao ki app har IP (0.0.0.0) aur is Port par chale
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+var app = builder.Build();
 var app = builder.Build();
 
 // Swagger (optional but useful)
