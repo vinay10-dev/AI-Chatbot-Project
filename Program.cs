9037578f2605d9c34d669b3ae73d8f9f56e086ef
vinay10-dev <vinaysingh769@gmail.com>
2026-03-31 14:38:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowAll", policy => {
         policy.AllowAnyOrigin()
@@ -16,16 +14,8 @@ builder.Services.AddCors(options => {
 
 var app = builder.Build();
 
-// Middleware
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseCors("AllowAll");
 
-// IMPORTANT: static files serve karne ke liye
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
